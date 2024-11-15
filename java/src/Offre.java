89d957a6a31afb2682fc2df6583e8a03c7d00222
Mathieu <77482141+Mathieeeu@users.noreply.github.com>
2024-11-15 08:29:51 +0100

    Update Offre.java

--- java/src/Offre.java
+++ java/src/Offre.java
@@ -1,7 +1,56 @@
-public class Offre {
-    private Materiel materiel;
+public abstract class Offre {
+    // Attributs 
+    private ArrayList<Personne> participants;
+    private Personne auteur;
+    private String datePublication;
+    private String description;
 
-    public Offre(Materiel materiel) {
-        this.materiel = materiel;
+    // Constructeur
+    public Offre(Personne auteur, String datePublication, String description) {
+        this.auteur = auteur;
+        this.datePublication = datePublication;
+        this.description = description;
+        this.participants = new ArrayList<Personne>();
+    }
+
+    // Getters et Setters
+    public ArrayList<Personne> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(ArrayList<Personne> participants) {
+        this.participants = participants;
+    }
+
+    public void addParticipant(Personne participant) {
+        this.participants.add(participant);
+    }
+
+    public void removeParticipant(Personne participant) {
+        this.participants.remove(participant);
+    }
+
+    public Personne getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(Personne auteur) {
+        this.auteur = auteur;
+    }
+
+    public String getDatePublication() {
+        return datePublication;
+    }
+
+    public void setDatePublication(String datePublication) {
+        this.datePublication = datePublication;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
