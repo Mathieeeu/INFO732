7f31e37bda10d4c70edda43f6ccd4cd48a1f98e7
Louna Camas <lounacamas@gmail.com>
2024-11-15 09:14:03 +0100

    Update Medecin.java

--- java/src/Medecin.java
+++ java/src/Medecin.java
@@ -1,3 +1,52 @@
+import java.util.ArrayList;
+
 public class Medecin {
-    
+
+    //Attribut
+    private static Medecin instance = null;
+    private Personne personne;
+    private ArrayList<Creneau> planning = new ArrayList<Creneau>();
+
+    //Constructeur
+    private Medecin(Personne personne){}
+
+    //Getters et setters
+    public static Medecin getInstance(Personne personne){
+        if (instance == null){
+            instance = new Medecin(personne);
+        }
+        return instance;
+    }
+
+    public ArrayList<Creneau> getPlanning(){
+        return planning;
+    }
+
+    public void setMedecin(Personne personne){
+        this.personne = personne;
+    }
+
+    //To string
+    public String ToString(){
+        String res = "";
+        for(Creneau creneau : planning){
+            res = res + creneau;
+        }
+        return res;
+    }
+
+    //Ajouter un RDV médical (méthode appelée par une personne)
+    public Creneau ajouterRDV(){
+        Creneau dernierCreneau = this.dernierCreneauOccupé();
+        Creneau newCreneau = new Creneau(dernierCreneau);
+        planning.add(newCreneau);
+        return newCreneau;
+    }
+
+    //Renvoie le dernier creneau occupé
+    public Creneau dernierCreneauOccupé(){
+        Creneau lastElement = planning.get(planning.size() - 1);
+        return lastElement;
+    }
+
 }
